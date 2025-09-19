3947685616112d1e316252143a694efbcbcf4218
etoundijuniorpierre <etoundijunior@hotmail.com>
2025-09-19 15:30:19 +0100

    feat : add notificationService

---
+++ springBoot/src/main/java/com/example/GestionClinique/service/NotificationService.java
@@ -0,0 +1,18 @@
+package com.example.GestionClinique.service;
+
+import com.example.GestionClinique.model.entity.Message;
+import com.example.GestionClinique.model.entity.Notification;
+import com.example.GestionClinique.model.entity.RendezVous;
+import com.example.GestionClinique.model.entity.Utilisateur;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface NotificationService {
+    Notification creerNotificationPourMessage(Message message, Utilisateur destinataire);
+    Notification creerNotificationPourRendezVous(RendezVous rendezVous, Utilisateur utilisateur);
+    List<Notification> getNotificationsByUtilisateur(Utilisateur utilisateur);
+    List<Notification> getUnreadNotifications(Utilisateur utilisateur);
+    void markAsRead(Long notificationId);
+}
